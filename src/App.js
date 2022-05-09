adbf6676a6babc70e5ee551658ba1a85c84c2497
Fiona <fiona_berkery@hotmail.com>
2022-05-09 16:01:26 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    const [tasks, setTasks] = useState([
+      { name: "Food Shop", isHighPriority: false },
+      { name: "Ironing", isHighPriority: true },
+      { name: "Wash car", isHighPriority: false }
+    ])
+
+    const [newTask, setNewTask] = useState("I am the new task - I am default")
+    const [newTaskPriority, setNewTaskPriority] = useState(false)
+
+
+    const taskItems = tasks.map((task, index) => {
+      return (
+        <li key={index} className= {task.isHighPriority ? "high" : "low"}>
+        
+        <span>{task.name}</span>
+        </li>
+      )
+    })
+
+    const handleTaskInput = (event) => {
+      setNewTask(event.target.value)
+    }
+
+    const handleRadioInput = (event) => {
+      setNewTaskPriority(event.target.value)
+    }
+    
+    const saveNewTask = ((event) => {
+      event.preventDefault()
+      const copyTask = [... tasks]
+      copyTask.push({name: newTask, isHighPriority: newTaskPriority})
+      setTasks(copyTask)
+
+      setNewTask("")
+    })
+    
+  
+    return (
+      <div className="App">
+
+      
+      <h1> Fiona's To Do List </h1>
+      
+      <hr></hr>
+
+      <ul>
+        {taskItems}
+      </ul>
+
+      <form onSubmit={saveNewTask}>
+        <label htmlFor="new-task"> Add a new task : </label>
+        <input id="new-task" type="text" onChange={handleTaskInput} value={newTask} />
+        <input type="submit" value="Save New Task" />  
+
+        
+        <input onClick={handleRadioInput} type="radio" id="high" name="isHighPriority" value="high" />
+        <label for="high"> High Priority </label> 
+        <input onClick={handleRadioInput} type="radio" id="low" name="isHighPriority" value=""/>
+        <label for="low"> Low Priority </label>
+        
+        
+      </form>
+      </div>
+    )
 }
 
 export default App;
+
+
